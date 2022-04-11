4fbb15e94071893dc99f17e912e0b74fb7c1064f
marceloungaretti <marcelo.ungaretti@gmail.com>
2022-04-11 00:13:17 -0300

    Fim do capítulo 3.

--- src/main/java/br/com/alura/loja/modelo/Pedido.java
+++ src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -17,7 +17,7 @@ public class Pedido {
     private BigDecimal valorTotal = BigDecimal.ZERO;
     private LocalDate data = LocalDate.now();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Cliente cliente;
 
     @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
@@ -66,5 +66,13 @@ public class Pedido {
 
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
+    }
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemPedido> itens) {
+        this.itens = itens;
     }
 }
